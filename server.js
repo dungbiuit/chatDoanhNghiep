3a62f725bfbb901b480dd58b1632d6070e541040
Hau <31406526+tronghau2110@users.noreply.github.com>
2020-05-02 15:59:46 +0700

    Hau : file server update inbox

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 let express = require("express");
-
 let body_parser = require("body-parser");
 let app = express();
+//
+users=[];
+connections=[];
 
 app.use(express.static("public"));
 app.use(body_parser.urlencoded({ extended: false }));
@@ -13,19 +15,49 @@ let server = require("http").Server(app);
 let io = require("socket.io")(server);
 server.listen(3000);
 
-//Biến toàn cục quan trọng
-let usersArray = [];
-
 io.on("connection", (socket) =>{
 	console.log("có người log vào với id là  " + socket.id);
 
 	socket.on("client-send-sign-in", datareceive => {
-		console.log("tên người dùng này là " + datareceive);
+	console.log("tên người dùng này là " + datareceive);
 	});
 
+//	socket.on("client-send-sign-up", userreceive  => {
+	//	console.log("username: " + userreceive.username);
+//	});
 
-})
-//Điều hướng app (routes)
+});
+//Chat 
+io.sockets.on('connection', function(socket){
+    connections.push(socket);
+    console.log('Connected: %s sockets connected', connections.length);
+
+    // Disconnect
+    socket.on('disconnect', function(data){
+        users.splice(users.indexOf(socket.username), 1);
+        updateUsernames();
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnected: %s socket connected', connections.length)
+    });
+    // Send message
+    socket.on('send message', function(data){
+        console.log(data);
+        io.sockets.emit('new message', {msg: data, user: socket.username});
+    });
+
+    // New User
+    socket.on('new user', function(data, callback){
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsernames();
+    });
+
+    function updateUsernames() {
+        io.sockets.emit('get users', users);
+    }
+
+});
 //Route đến trang chủ khi nhập localhost:3000
 app.get("/", (request, respond) => {
 	respond.render("trangchu");
@@ -34,60 +66,15 @@ app.get("/", (request, respond) => {
 app.get("/trangdangky", (request,respond) => {
 	respond.render("trangdangky");
 });
-//Xử lý các điều kiện đăng ký thành viên 
 app.post("/trangdangky", (request,respond) => {
-	let userReceive = returnUserAfterClickSignUp(request);
-	console.log(userReceive);
-	let webPageRender = returnWebPageAfterCheckPassword(userReceive.firstTypePassword, userReceive.retypePassword);
-	respond.redirect(webPageRender);
-	addUserToUserArray(userReceive, usersArray);
-	console.log(usersArray);
-});
+console.log(JSON.stringify(request.body));	
+//respond.redirect("/");
 
+});
 app.get("/post-sign-in", (request,respond) => {
 	respond.render("post-sign-in");
 });
-// Hàm xử lý 
-const returnWebPageAfterCheckPassword = (firstPassword, retypePassword) =>{
-	let webPageReturn = "";
-	if(checkRetypePassword(firstPassword, retypePassword)){
-		webPageReturn = "/"
-	}
-	else
-		webPageReturn = "trangdangky";
-	return webPageReturn;
-}
+app.get("/inbox", (request,respond) => {
+	respond.render("inbox");
+});
 
-const checkRetypePassword = (firstPassword, retypePassword) => {
-	if(firstPassword !== retypePassword){
-		return false;
-	}
-	return true;
-}
-
-
-const returnUserAfterClickSignUp = (request) => {
-	let userReturn = JSON.stringify(request.body);	
-	userReturn = JSON.parse(userReturn);
-	return userReturn;	
-}
-
-const addUserToUserArray = (userAdd, userArray) => {
-	if(checkEmailExistionOfUserAdd(userAdd, userArray) === false 
-		&&
-		checkRetypePassword(userAdd.firstTypePassword, userAdd.retypePassword) ) {
-
-		userArray.push(userAdd);
-	}
-	else
-		console.log("Thêm thất bại");
-}
-
-const checkEmailExistionOfUserAdd = (userAdd, userArray) => {
-	for (eachUser of userArray) {
-		if(userAdd.emailSignUp === eachUser.emailSignUp) {
-			return true;
-		}
-	}
-	return false;
-}
