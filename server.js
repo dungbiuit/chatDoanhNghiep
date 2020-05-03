3017c99673858128c136cd70e7577e81ff4c17e4
dungbiuit <dungnguyen.bill@gmail.com>
2020-05-03 08:03:33 +0700

    Fix đường dẫn khi bắt đầu vào localhost sẽ là trang inbox của Hậu

--- server.js
+++ server.js
@@ -19,62 +19,59 @@ io.on("connection", (socket) =>{
 	console.log("có người log vào với id là  " + socket.id);
 
 	socket.on("client-send-sign-in", datareceive => {
-	console.log("tên người dùng này là " + datareceive);
+		console.log("tên người dùng này là " + datareceive);
 	});
 
-//	socket.on("client-send-sign-up", userreceive  => {
+	//	socket.on("client-send-sign-up", userreceive  => {
 	//	console.log("username: " + userreceive.username);
-//	});
+	//	});
 
 });
 //Chat 
 io.sockets.on('connection', function(socket){
-    connections.push(socket);
-    console.log('Connected: %s sockets connected', connections.length);
+	connections.push(socket);
+	console.log('Connected: %s sockets connected', connections.length);
 
-    // Disconnect
-    socket.on('disconnect', function(data){
-        users.splice(users.indexOf(socket.username), 1);
-        updateUsernames();
-        connections.splice(connections.indexOf(socket), 1);
-        console.log('Disconnected: %s socket connected', connections.length)
-    });
-    // Send message
-    socket.on('send message', function(data){
-        console.log(data);
-        io.sockets.emit('new message', {msg: data, user: socket.username});
-    });
+	// Disconnect
+	socket.on('disconnect', function(data){
+		users.splice(users.indexOf(socket.username), 1);
+		updateUsernames();
+		connections.splice(connections.indexOf(socket), 1);
+		console.log('Disconnected: %s socket connected', connections.length)
+	});
+	// Send message
+	socket.on('send message', function(data){
+		console.log(data);
+		io.sockets.emit('new message', {msg: data, user: socket.username});
+	});
 
-    // New User
-    socket.on('new user', function(data, callback){
-        callback(true);
-        socket.username = data;
-        users.push(socket.username);
-        updateUsernames();
-    });
+	// New User
+	socket.on('new user', function(data, callback){
+		callback(true);
+		socket.username = data;
+		users.push(socket.username);
+		updateUsernames();
+	});
 
-    function updateUsernames() {
-        io.sockets.emit('get users', users);
-    }
+	function updateUsernames() {
+		io.sockets.emit('get users', users);
+	}
 
 });
 //Route đến trang chủ khi nhập localhost:3000
 app.get("/", (request, respond) => {
-	respond.render("trangchu");
+	respond.render("inbox");
 });
 //Route đến trang đăng ký khi nhấn vào link đăng ký trong trang chủ
 app.get("/trangdangky", (request,respond) => {
 	respond.render("trangdangky");
 });
 app.post("/trangdangky", (request,respond) => {
-console.log(JSON.stringify(request.body));	
-//respond.redirect("/");
+	console.log(JSON.stringify(request.body));	
+	//respond.redirect("/");
 
 });
 app.get("/post-sign-in", (request,respond) => {
 	respond.render("post-sign-in");
-});
-app.get("/inbox", (request,respond) => {
-	respond.render("inbox");
 });
 
