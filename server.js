1a9312fc6d68807d3f441d491e60668885f98ce9
dungbiuit <dungnguyen.bill@gmail.com>
2020-05-06 16:13:38 +0700

    Them chuc nang load vao room va message

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ let body_parser = require("body-parser");
 let app = express();
 
 //
-users=[];
-connections=[];
+users = [];
+connections = [];
 
 app.use(express.static("public"));
 app.use(body_parser.urlencoded({ extended: false }));
@@ -14,17 +14,21 @@ app.set("views", "./views");
 
 let server = require("http").Server(app);
 let io = require("socket.io")(server);
-server.listen(3000);
+server.listen(process.env.PORT || 3000);
+
 // connect socket
-io.on("connection", (socket) =>{
+io.on("connection", socket => {
 	console.log("có người log vào với id là  " + socket.id);
-
-
 });
+
 //Chat 
 io.sockets.on('connection', function(socket){
 	connections.push(socket);
 	console.log('Connected: %s sockets connected', connections.length);
+
+	//Cho socketID vao mang Users
+	users.push(socket.id);
+	updateUsernames();
 	
 	// Disconnect socket
 	socket.on('disconnect', function(data){
@@ -33,24 +37,15 @@ io.sockets.on('connection', function(socket){
 		connections.splice(connections.indexOf(socket), 1);
 		console.log('Disconnected: %s socket connected', connections.length)
 	});
-	
-	// Send message
-	socket.on('send message', function(data){
-		
-		io.sockets.emit('new message', {msg: data, user: socket.id});
-	});
-
-	// Tạo users
-	socket.on('new user', function(data, callback){
-		callback(true);
-	//	socket.username = data;
-		users.push(socket.id);
-		updateUsernames();
-	});
 
 	function updateUsernames() {
 		io.sockets.emit('get users', users);
 	}
+
+	// Send message
+	socket.on('send message', function(data){
+		io.sockets.emit('new message', {msg: data, user: socket.id});
+	});
 
 });
 //Route đến trang chủ khi nhập localhost:3000
